47b01b67517a12d74db06d29b6c084382100521a
huanghao <huanghao@yy.com>
2016-01-07 12:04:56 +0800

    Fix Authentication refused: bad ownership or modes for directory /data

--- docker/add-ssh-key.sh
+++ docker/add-ssh-key.sh
@@ -3,6 +3,9 @@
 # The /data may be externally mounted, so it is empty at this time!
 # We cp the content from /root without overwrite some existing files!
 cp -Rn /root/. /data
+# Externally mounted dir has mod 777, which prohibits ssh auth.
+# The home directory should be writable only by root!
+chmod go-w /data
 
 # We add SSH_PUBLIC_KEY only if the user specified it and it was not added!
 if [ -n "$SSH_PUBLIC_KEY" ]; then
