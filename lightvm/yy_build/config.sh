e6a0530bcbd4badbab06963467893fecf2b8607e
Homer Huang <aclisp@gmail.com>
2016-01-11 15:00:22 +0800

    Change apt source to mirror.

--- lightvm/yy_build/config.sh
+++ lightvm/yy_build/config.sh
@@ -1,13 +1,12 @@
 cat > /etc/apt/sources.list << EOF
-deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty main restricted universe multiverse
-deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-security main restricted universe multiverse
-deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-updates main restricted universe multiverse
-deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-proposed main restricted universe multiverse
-deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-backports main restricted universe multiverse
-deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-prisoft main
-deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty main restricted universe multiverse
-deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty-security main restricted universe multiverse
-deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty-updates main restricted universe multiverse
-deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty-proposed main restricted universe multiverse
-deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty-backports main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-security main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-proposed main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-backports main restricted universe multiverse
+deb-src mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse
+deb-src mirror://mirrors.ubuntu.com/mirrors.txt trusty-security main restricted universe multiverse
+deb-src mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main restricted universe multiverse
+deb-src mirror://mirrors.ubuntu.com/mirrors.txt trusty-proposed main restricted universe multiverse
+deb-src mirror://mirrors.ubuntu.com/mirrors.txt trusty-backports main restricted universe multiverse
 EOF
