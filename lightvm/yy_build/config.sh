8750437aa0efadeb0e00d031bbd8af2b6560fe12
huanghao <huanghao@yy.com>
2016-01-11 14:24:31 +0800

    Add yy_build scripts.

---
+++ lightvm/yy_build/config.sh
@@ -0,0 +1,13 @@
+cat > /etc/apt/sources.list << EOF
+deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty main restricted universe multiverse
+deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-security main restricted universe multiverse
+deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-updates main restricted universe multiverse
+deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-proposed main restricted universe multiverse
+deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-backports main restricted universe multiverse
+deb http://mirror.yy.duowan.com:63782/ubuntu/ trusty-prisoft main
+deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty main restricted universe multiverse
+deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty-security main restricted universe multiverse
+deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty-updates main restricted universe multiverse
+deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty-proposed main restricted universe multiverse
+deb-src http://mirror.yy.duowan.com:63782/ubuntu/ trusty-backports main restricted universe multiverse
+EOF
