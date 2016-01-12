05c5aeae0920d164c5d599cfe32560800b316f7f
huanghao <huanghao@yy.com>
2016-01-12 11:17:06 +0800

    Temp commit.

--- launch-vm.js
+++ launch-vm.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var req = fs.readFileSync('lightvm.json', 'utf8');
 
-req.replace(/@REGISTRY@/g, "61.160.36.122:8080")
+req = req.replace(/@REGISTRY@/g, "61.160.36.122:8080")
     .replace(/@IMAGE_NAME@/g, process.env.IMAGE_NAME)
     .replace(/@IMAGE_VERSION@/g, process.env.IMAGE_VERSION)
     .replace(/@SSH_PORT@/g, process.env.SSH_PORT)
