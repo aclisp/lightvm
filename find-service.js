33845e6194e52b3f37cf40cb248c75dbd34f9ab3
Homer Huang <aclisp@gmail.com>
2016-01-20 16:43:03 +0800

    Add find-service.

---
+++ find-service.js
@@ -0,0 +1,30 @@
+var https = require('https');
+var querystring = require('querystring');
+
+var unique_name = "vm-" + process.env.BUILD_USER_ID + "-" + process.env.SERVICE_NAME;
+var query = {
+    labelSelector: "user=" + process.env.BUILD_USER_ID + "," +
+                   "name=" + unique_name
+};
+
+var options = {
+    hostname: '61.160.36.122',
+    port: 443,
+    path: '/api/v1/namespaces/default/pods?' + querystring.stringify(query),
+    method: 'GET',
+    auth: 'test:test123',
+    rejectUnauthorized: false
+};
+var req = https.request(options, function (res) {
+    if (res.statusCode != 200) {
+        res.on('data', function (chunk) {
+            var status = JSON.parse(chunk);
+            throw new Error(status.message);
+        });
+    }
+    res.on('data', function (chunk) {
+        var obj = JSON.parse(chunk);
+        console.log(obj);
+    });
+});
+req.end();
