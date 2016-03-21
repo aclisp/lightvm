9f4465d8c72207e44e69dd40cd59989002b7ee8b
Homer Huang <aclisp@gmail.com>
2016-03-21 13:56:43 +0800

    Scale can input >9.

--- sigma-sdk/scale.sh
+++ sigma-sdk/scale.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source functions.sh
 CURRENT_REPLICAS=$(read_replication_controller $CLUSTER_NAME | jq '.spec.replicas')
-read -p "! About to scale replication controller $CLUSTER_NAME ! Current replicas $CURRENT_REPLICAS ! Input required replicas: " -n 1 -r
+read -p "! About to scale replication controller $CLUSTER_NAME ! Current replicas $CURRENT_REPLICAS ! Input required replicas: " -r
 echo ""
 if [[ $REPLY =~ ^[0-9]+$ ]]; then
     echo "Required replicas $REPLY"
