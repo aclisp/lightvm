78125ddc03865b472a04c2ac75ebf03a0b347909
Homer Huang <aclisp@gmail.com>
2016-05-19 10:51:44 +0800

    ssh now supports hostNetwork pods.

--- sigma-sdk/bash/ssh.sh
+++ sigma-sdk/bash/ssh.sh
@@ -20,14 +20,20 @@ if [[ -z $INSTANCE_NODE ]]; then
     exit 1
 fi
 
-INSTANCE_STATUS=$(echo $INSTANCE_SPEC | jq --raw-output '.status.message')
-PORT_MAPPINGS=$(echo $INSTANCE_STATUS | sed -r 's/PortMapping\((.*)\)/\1/' | tr ',' ' ')
-SSH_PORT_PATTERN='^([0-9]+)->22/TCP$'
-for port_mapping in $PORT_MAPPINGS; do
-    if [[ $port_mapping =~ $SSH_PORT_PATTERN ]]; then
-        OUTER_PORT=${BASH_REMATCH[1]}
-    fi
-done
+HOST_NETWORK=$(echo $INSTANCE_SPEC | jq --raw-output '.spec.hostNetwork')
+if [[ $HOST_NETWORK == "true" ]]; then
+    OUTER_PORT=$(echo $INSTANCE_SPEC | jq --raw-output '.spec.containers[0].ports[0].hostPort')
+else
+    INSTANCE_STATUS=$(echo $INSTANCE_SPEC | jq --raw-output '.status.message')
+    PORT_MAPPINGS=$(echo $INSTANCE_STATUS | sed -r 's/PortMapping\((.*)\)/\1/' | tr ',' ' ')
+    SSH_PORT_PATTERN='^([0-9]+)->22/TCP$'
+    for port_mapping in $PORT_MAPPINGS; do
+        if [[ $port_mapping =~ $SSH_PORT_PATTERN ]]; then
+            OUTER_PORT=${BASH_REMATCH[1]}
+        fi
+    done
+fi
+
 if [[ -z $OUTER_PORT ]]; then
     >&2 echo "Can not capture SSH port for instance $INSTANCE_NAME: $INSTANCE_STATUS"
     exit 1
